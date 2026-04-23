53e06a495b91d3cc5ff85343fd95b0a2319a5f63
holden.anderson <holden.anderson@students.snow.edu>
2026-04-23 17:40:17 -0600

    finished program

--- O47CreateExtensionMethod/Program.cs
+++ O47CreateExtensionMethod/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    static void Main()
+    {
+        int Number = 7;
+        Console.WriteLine(Number.IsEven());
+        Console.WriteLine(8.IsEven());
+        Number = 46;
+        Console.WriteLine(Number.IsEven());
+    }
+
+}
+
+public static class EvenChecker
+{
+    public static bool IsEven(this int Number)
+    {
+        if (Number % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
